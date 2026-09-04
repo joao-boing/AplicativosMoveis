5193e4cfc75b35d3e1a1560c07734aa3f5ea994b
unknown <joaovtboing@gmail.com>
2026-09-04 11:59:30 -0300

    Creating new activity

--- boingAplication/app/src/main/java/com/example/boingaplication/MainActivity.java
+++ boingAplication/app/src/main/java/com/example/boingaplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.boingaplication;
+import android.content.Intent;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -10,24 +12,61 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
-    int contador = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Button b = findViewById(R.id.button);
-        TextView tx = findViewById(R.id.txt);
-        b.setOnClickListener(view -> {
-            contador ++;
-            tx.setText(Integer.toString(contador));
-        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+        });
+
+        EditText edMin,edMax;
+        edMin = findViewById(R.id.edMax);
+        edMax = findViewById(R.id.edMax);
+
+        TextView textView=findViewById(R.id.textView);
+        Button b=findViewById(R.id.button);
+
+        b.setOnClickListener(v -> {
+
+            Random random = new Random();
+            String smax= edMax.getText().toString();
+            String smin= edMin.getText().toString();
+
+            if(smin.trim().equals("")){
+                edMin.setError("Informe um Valor");
+                edMin.requestFocus();
+                return;
+            }
+            if(smax.trim().equals("")){
+                edMax.setError("Informe um valor");
+                edMax.requestFocus();
+                return;
+            }
+
+            int min  = Integer.parseInt(edMin.getText().toString());
+            int max = Integer.parseInt(edMax.getText().toString());
+
+            if(min>max){
+                edMax.setError("O valor máximo deve ser maior que o mínimo");
+                return;
+
+            }
+
+            int r = (random.nextInt( max-min))+min;
+            textView.setText(Integer.toString(r));
+
+            Intent intent = new Intent(MainActivity.this, activity_2.class);
+            intent.putExtra("NUMERO_SORTEADO", r);
+            startActivity(intent);
         });
     }
 }
